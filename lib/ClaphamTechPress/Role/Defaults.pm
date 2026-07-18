d2a96a52b3056cfe770267a6428347045685cca1
Dave Cross <dave@davecross.co.uk>
2026-07-18 11:44:05 +0100

    New "Defaults" role

---
+++ lib/ClaphamTechPress/Role/Defaults.pm
@@ -0,0 +1,58 @@
+package ClaphamTechPress::Role::Defaults;
+
+use v5.20;
+use experimental 'signatures';
+use Moo::Role;
+
+requires qw[slug];
+
+sub domain ($self) {
+  return 'https://claphamtechpress.com';
+}
+
+sub og_url_path ($self) {
+  return '/books/' . $self->slug;
+}
+
+sub og_url ($self) {
+  return $self->domain_url . $self->og_url_path;
+} 
+
+sub og_site_name ($self) {
+  return 'Clapham Technical Press';
+}
+
+sub og_title ($self) {
+  return 'Clapham Techical Press';
+}
+
+sub og_description ($self) {
+  return 'No description!';
+}
+
+sub og_image ($self) {
+  my $url = $self->domain_url;
+  my $image;
+
+  if ($self->can('image') and $self->image) {
+    $image = $self->image;
+  } else {
+    $image = '/images/booker2024-short.jpg';
+  }
+
+  if ($image !~ m|^https?://|) {
+    $image = "$url$image";
+  }
+
+  return $image;
+}
+
+sub og_type {
+  return 'website';
+}
+
+sub no_index {
+  return 0;
+}
+
+1;
